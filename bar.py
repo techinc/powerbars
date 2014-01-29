090f22a4af0e650485b7654439066c3216d92a33
Merlijn Wajer <merlijn@wizzup.org>
2014-01-29 14:56:27 +0100

    Add example configuration, break working code
    ... Will fix tonight.

--- bar.py
+++ bar.py
@@ -1,5 +1,17 @@
 import time
-from serial import Serial
+
+VIRTUAL = True
+
+if not VIRTUAL:
+    from serial import Serial
+else:
+    print 'WARNING: Virtual mode activated'
+    class Serial(object):
+        def __init__(self, **kwargs):
+            pass
+
+        def __getattr__(self, attr):
+            return lambda self, **kwargs: None
 
 def write_read(s, m):
     s.write(m)
@@ -13,8 +25,10 @@ def write_read(s, m):
     return f
 
 
+
+
 class PowerBar(object):
-    def __init__(self, path, sn):
+    def __init__(self, path, sn, name):
         s = Serial(port=path, baudrate=9600)
 
         # Just in case
@@ -24,11 +38,31 @@ class PowerBar(object):
 
         self.s = s
         self.sockets = [PowerSocket(self, _) for _ in xrange(sn)]
+        self.name = name
+
+    def __len__(self):
+        return len(self.sockets)
+
+    def __getitem__(self, index):
+        return self.sockets[index - 1] # XXX: YAFOOOO
+
+    def __iter__(self):
+        return iter(self.sockets)
 
 class PowerSocket(object):
-    def __init__(self, bar, num):
+    def __init__(self, bar, num, name=None):
+        """
+        Name is purely for descriptive purposes
+        """
         self.bar = bar
         self.num = num + 1
+        self.name = 'Socket %d' % self.num if not name else name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return 'PowerSocket (%s)' % self.name
 
     def set_state(self, state):
         s = 'On' if state else 'Off'
--- barconfig.py
+++ barconfig.py
@@ -2,4 +2,22 @@ from bar import PowerBar, write_read
 
 bars = []
 
-bars.append(PowerBar('/dev/ttyUSB0', 20))
+bars.append(PowerBar('/dev/ttyUSB0', 2, 'My Little Powerbar (MLP)'))
+
+FIRST_BAR = bars[0]
+
+LIGHT_TABLE                     = FIRST_BAR[0]
+LIGHT_SOLDERING                 = FIRST_BAR[2]
+
+groups = {
+    'lights' : [LIGHT_TABLE, LIGHT_SOLDERING],
+}
+
+GROUPS_LIGHT = groups['lights']
+
+prefixes = {
+    'spacestate' : [
+        GROUPS_LIGHT,
+    ]
+
+}
