fcf583bc332ed2eb1bc9eb5650477cc02cb004b9
Merlijn Wajer <merlijn@wizzup,org>
2014-02-26 00:39:35 +0100

    Add second powerbar.

--- barconfig.py
+++ barconfig.py
@@ -7,19 +7,23 @@ from bar import PowerBar, write_read
 bars = []
 
 FIRST_BAR = PowerBar('/dev/ttyS0', 20, 'My Little Powerbar (MLP)')
+SECOND_BAR = PowerBar('/dev/ttyUSB0', 20, 'My Large Powerbar (MLP)')
 
 bars += [
     FIRST_BAR,
+    SECOND_BAR
 ]
+
+# Bar 1
 
 LIGHT_SOLDER            = FIRST_BAR[3]
 LIGHT_BAR               = FIRST_BAR[4]
 LIGHT_TABLE             = FIRST_BAR[5]
 LIGHT_3D                = FIRST_BAR[6]
 LIGHT_ENTRANCE          = FIRST_BAR[7]
-LIGHT_X                 = FIRST_BAR[8]
-LIGHT_Y                 = FIRST_BAR[9]
-LIGHT_Z                 = FIRST_BAR[10]
+LIGHT_KITCHEN           = FIRST_BAR[8]
+#LIGHT_Y                 = FIRST_BAR[9]
+LIGHT_SOFA              = FIRST_BAR[10]
 
 AUDIO_AMPLIFIER         = FIRST_BAR[13]
 AUDIO_MIXER             = FIRST_BAR[14]
@@ -31,19 +35,32 @@ PRINTER                 = FIRST_BAR[18]
 MONITOR_AV_1            = FIRST_BAR[19]
 MONITOR_AV_2            = FIRST_BAR[20]
 
+# Bar 2 (New space)
+
+POWER_NW_TABLE          = SECOND_BAR[2]
+POWER_NE_TABLE          = SECOND_BAR[3]
+POWER_SW_TABLE          = SECOND_BAR[7]
+POWER_PILLAR            = SECOND_BAR[20]
+
+LIGHT_NE_TABLE          = SECOND_BAR[6]
+LIGHT_THEATHER          = SECOND_BAR[17]
+LIGHT_GLASS             = SECOND_BAR[19]
+
 groups = {
     'entrance' : [LIGHT_ENTRANCE],
     'solder' : [LIGHT_SOLDER],
     'bar' : [LIGHT_BAR],
     'table' : [LIGHT_TABLE],
-    'x' : [LIGHT_X],
-    'y' : [LIGHT_Y],
-    'z' : [LIGHT_Z],
+    'kitchen' : [LIGHT_KITCHEN],
+    'sofa' : [LIGHT_SOFA],
     'audio' : [AUDIO_AMPLIFIER, AUDIO_MIXER],
     '3d' : [LIGHT_3D, MONITOR_3D_1, MONITOR_3D_2],
     'displays' : [MONITOR_AV_1, MONITOR_AV_2, MONITOR_3D_1, MONITOR_3D_2],
     'av' : [MONITOR_AV_1, MONITOR_AV_2],
     'printer' : [PRINTER],
+    'powerwest' : [POWER_NW_TABLE, POWER_NE_TABLE, POWER_SW_TABLE,
+        POWER_PILLAR],
+    'lightwest' : [LIGHT_NE_TABLE, LIGHT_THEATHER, LIGHT_GLASS],
 }
 
 
@@ -51,7 +68,8 @@ groups_state = {}
 for _ in groups.iterkeys():
     groups_state[_] = None
 
-GROUPS_LIGHT = ['entrance', 'solder', 'bar', 'table', '3d', 'x', 'y', 'z']
+GROUPS_LIGHT = ['entrance', 'solder', 'bar', 'table', '3d', 'sofa', 'kitchen',
+        'lightwest']
 
 presets = {
     'lightsoff' : {
