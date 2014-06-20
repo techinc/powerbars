ae9d75f2f1e4ba5d992822d504271dfede54be5d
Merlijn Wajer <merlijn@wizzup,org>
2014-06-20 19:10:03 +0200

    Upgrade reset script.

--- reset.py
+++ reset.py
@@ -5,48 +5,60 @@ from __future__ import print_function
 from serial import Serial
 
 import time
+import sys
 
-def timeread(s):
-    s.timeout = 6
-    return s.read(10000)
+def resetserial(name):
 
-def serialwrite(s, m):
-    s.write(m + '\r\n')
+    def timeread(s):
+        s.timeout = 6
+        return s.read(10000)
+    
+    def serialwrite(s, m):
+        s.write(m + '\r\n')
+    
+    s = Serial(port='/dev/ttyS1', baudrate=9600)
+    
+    s.write('\r\n')
+    m = timeread(s)
+    #print(repr(m))
+    
+    s.write('Config\r\n')
+    m = timeread(s)
+    #print(repr(m))
+    
+    s.write('3\r\n')
+    m = timeread(s)
+    
+    #print(m)
+    
+    if 'Disable Confirmation' in m:
+        #print('Found disable')
+        s.write('Y\r\n')
+    else:
+        s.write('N\r\n')
+    
+    m = timeread(s)
+    #print(repr(m))
+    
+    s.write('4\r\n')
+    m = timeread(s)
+    
+    #print(m)
+    
+    if 'Disable Status Menu' in m:
+        #print('Found disable')
+        s.write('Y\r\n')
+    else:
+        s.write('N\r\n')
+    
+    m = timeread(s)
+    #print(repr(m))
+    
+    # Return to 'normal'
+    s.write('\r\n')
+    m = timeread(s)
+    #print(repr(m))
 
-s = Serial(port='/dev/ttyS1', baudrate=9600)
+if __name__ == '__main__':
+    resetserial(sys.argv[1])
 
-s.write('\r\n')
-print(repr(timeread(s)))
-
-s.write('Config\r\n')
-print(repr(timeread(s)))
-
-s.write('3\r\n')
-m = timeread(s)
-
-print(m)
-
-if 'Disable Confirmation' in m:
-    print('Found disable')
-    s.write('Y\r\n')
-else:
-    s.write('N\r\n')
-
-print(repr(timeread(s)))
-
-s.write('4\r\n')
-m = timeread(s)
-
-print(m)
-
-if 'Disable Status Menu' in m:
-    print('Found disable')
-    s.write('Y\r\n')
-else:
-    s.write('N\r\n')
-
-print(repr(timeread(s)))
-
-# Return to 'normal'
-s.write('\r\n')
-print(repr(timeread(s)))
